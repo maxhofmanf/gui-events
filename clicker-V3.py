6ffeeb5aae0ccf99a7b2b89af102b187f838d663
maxhofmanf <99068281@mydavinci.nl>
2022-03-25 11:28:05 +0100

    clickerv3

---
+++ clicker-V3.py
@@ -0,0 +1,57 @@
+import tkinter as tk
+from tkinter import *
+window = tk.Tk()
+counter = tk.IntVar(0)
+count=0
+stat = True
+
+def double(e):
+    global count
+    if stat == True:
+        count*=3
+        counter.set(count)
+    else:
+        count/=3
+        counter.set(count)
+def enter(e):
+    window['bg']='yellow'
+
+def Click1():
+    global count, stat
+    stat = True
+    count += 1
+    counter.set(count)
+    background2()
+def Click2():
+    global count,stat
+    stat = False
+    count -=1
+    counter.set(count)
+    background2()
+box1 = tk.Label(
+    window,
+    textvariable= counter,
+    width = 9,
+    height = 2
+
+)
+def background1(e):
+    background2()
+def background2():
+    if counter.get() > 0 :
+        window['bg']= 'green'
+    elif counter.get() < 0:
+        window['bg']='red'
+    else:
+        window['bg']='gray'
+
+btn1= tk.Button(text='+' ,bg="white", fg="black", padx = 30 , pady = 10, command= Click1)
+btn2= tk.Button(text='-' ,bg="white", fg="black", padx = 30 , pady = 10, command= Click2)
+btn1.pack()
+box1.pack()
+btn2.pack()
+
+box1.bind('<Enter>',enter)
+box1.bind('<Leave>',background1)
+box1.bind('<Double-Button-1>',double)
+window.mainloop()
